4e7ceb7421d426e669255a99141e1035c5d76a9f
HassanSherwani <45789548+HassanSherwani@users.noreply.github.com>
2020-04-18 15:43:56 +0200

    update code

--- NER_Streamlit/app.py
+++ NER_Streamlit/app.py
@@ -492,5 +492,151 @@ def main():
     if choice == 'Extract MetaData from .pdf File':
         st.subheader("Extract MetaData from .pdf File")
 
+        def file_selector(folder_path='.'):
+            filenames = os.listdir(folder_path)
+            selected_filename = st.selectbox('Please only Select a .docx File', filenames)
+            return os.path.join(folder_path, selected_filename)
+
+        filename = file_selector()
+
+        f = open(filename)
+        st.write('You have selected `%s`' % filename)
+
+        with open(filename, "rb") as pdf:
+            pdf_file = pdftotext.PDF(pdf)
+        # converting pdftotext.PDF to string type
+        pdf_text = ("\n\n".join(pdf_file))
+        raw_text4 = st.text_area("your file contains following text", pdf_text)  # for storing in raw text
+
+        # DEFINE ANALYISIS FUNCTION
+
+        def analyze_text(text):
+            return nlp(text)
+        # Applying NLP ideas
+
+        title1 = ["Agreement on Managed Data Center Services"]
+        title2 = ["Master Services Agreement on the Provision of IT Services",
+                      "Master Services Agreement on the Provision of IT Services (“Agreement“ or “Master Services Agreement”)"]
+        title3 = ["MASTER SERVICES AGREEMENT ON THE PROVISION OF MANAGED SERVICES IN PUBLIC COULDS",
+                      "Master Services Agreement (“Agreement“ or “Master Services Agreement”) on the provision of Managed Services in Public Clouds"]
+        title4 = ["Agreement on the Provision of MANAGED PRINT Services",
+                      "Agreement on the Provision of MPS (Managed Print Services)"]
+        title5 = ["Agreement for Security Operation Center Services"]
+        title6 = ["AGREEMENT ON PROVISIONING OF IT AND COMMUNICATION SERVICES"]
+        title7 = ["Agreement on Managed Data Center Services"]
+        title8 = ["Master Project, Support and Maintenance Agreement"]
+        title9 = ["ENTERPRISE CUSTOMER AGREEMENT"]
+
+
+        suppliers1 = ["TEASYS", "Teasys", "TEASYS GLOBAL INVEST AG", "Teasys Global Invest AG"]
+        suppliers2 = ["FTP", "FTP Deutschland GmbH", "FTP Deutschland GmbH"]
+        suppliers3 = ["Wisniewski & Sohn GmbH", "FBS"]
+        suppliers4 = ["Horizon Deutschland AG", "Horizon", "Harpe", "Harpe Deutschland GmbH"]
+        suppliers5 = ["ADVENTURE SERVICES GMBH", "Adventure Services GmbH", "SWIPERO LIMITED", "Swipero Limited",
+                          "Swipero"]
+
+        clients = ["F.UN", "FUN", "F.UN BUSINESS SERVICES GMBH", "F.UN Business Services GmbH"]
+
+        dates1 = ["29 September 2018", "01 January 2015", "01.07.2018", " August 2017"]
+        dates2 = ["31. July 2018"]
+        dates3 = ["termination after a period of 48 months"]
+        dates4 = ["31.01.2017", "31.03.2019", "1 October 2018"]
+        dates5 = ["31.12.2018", "Apr 11th 2023", "19.01.2020"]
+
+        countries1 = ["UK", "Germany", "France", "Italy", "Netherlands", "Russia", "Hungary", "India"]
+        countries2 = ["Slovakia", "Czech", "Australia", "Vietnam", "Japan", "Philippines", "Romania"]
+
+        # Define rulerAll for all entities
+        rulerAll = EntityRuler(nlp, overwrite_ents=True)
+
+        # Add all patterns in respective entity
+        for tit1 in title1:
+            rulerAll.add_patterns([{"label": "TITLE", "pattern": tit1}])
+
+        for tit2 in title2:
+            rulerAll.add_patterns([{"label": "TITLE", "pattern": tit2}])
+
+        for tit3 in title3:
+            rulerAll.add_patterns([{"label": "TITLE", "pattern": tit3}])
+
+        for tit4 in title4:
+            rulerAll.add_patterns([{"label": "TITLE", "pattern": tit4}])
+
+        for tit5 in title5:
+            rulerAll.add_patterns([{"label": "TITLE", "pattern": tit5}])
+
+        for tit6 in title6:
+            rulerAll.add_patterns([{"label": "TITLE", "pattern": tit6}])
+
+        for tit7 in title7:
+            rulerAll.add_patterns([{"label": "TITLE", "pattern": tit7}])
+
+        for tit8 in title8:
+            rulerAll.add_patterns([{"label": "TITLE", "pattern": tit8}])
+
+        for tit9 in title9:
+            rulerAll.add_patterns([{"label": "TITLE", "pattern": tit9}])
+
+        # for supplier
+
+        for s1 in suppliers1:
+            rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s1}])
+
+        for s2 in suppliers2:
+            rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s2}])
+
+        for s3 in suppliers3:
+            rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s3}])
+
+        for s4 in suppliers4:
+            rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s4}])
+
+        for s5 in suppliers5:
+            rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s5}])
+
+        # for clients
+
+        for c1 in clients:
+            rulerAll.add_patterns([{"label": "CLIENT", "pattern": c1}])
+
+        # Pattern for DATES
+
+        for t1 in dates1:
+            rulerAll.add_patterns([{"label": "Effective-DATES", "pattern": t1}])
+
+        for t2 in dates2:
+            rulerAll.add_patterns([{"label": "Signature-DATES", "pattern": t2}])
+
+        for t3 in dates3:
+            rulerAll.add_patterns([{"label": "Termination-DATES", "pattern": t3}])
+
+        for t4 in dates4:
+            rulerAll.add_patterns([{"label": "Commencement-DATES", "pattern": t4}])
+
+        for t5 in dates5:
+            rulerAll.add_patterns([{"label": "END-DATES", "pattern": t5}])
+
+        # for countries
+
+        for count1 in countries1:
+            rulerAll.add_patterns([{"label": "COUNTRIES", "pattern": count1}])
+
+        for count2 in countries2:
+            rulerAll.add_patterns([{"label": "COUNTRIES", "pattern": count2}])
+
+        # Define Ruler for All
+
+        rulerAll.name = 'rulerAll'
+        nlp.add_pipe(rulerAll)
+
+        # analysis from loaded file
+
+        if st.button("Extract"):
+            document4 = analyze_text(raw_text4)
+            html = displacy.render(document4, style="ent")
+            html = html.replace("\n\n", "\n")
+            st.markdown(HTML_WRAPPER.format(html), unsafe_allow_html=True)
+
+
 if __name__ == '__main__':
     main()
